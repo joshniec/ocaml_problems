8f696066867a49d9d2ab4f963f977630928740e6
Josh Niec <ironick09@gmail.com>
2022-06-12 11:53:32 -0400

    formatting

--- src/last_two_elements_of_a_list.ml
+++ src/last_two_elements_of_a_list.ml
@@ -2,9 +2,9 @@
 let rec last_two (l : 'a list) : ('a * 'a) option =
   match l with
   | [] | [ _ ] -> None
-  | [x;y] -> Some (x, y)
+  | [ x; y ] -> Some (x, y)
   | _ :: rest -> last_two rest
 
 let%test "empty list" = last_two [] = None
-let%test "single item list" = last_two [1] = None
+let%test "single item list" = last_two [ 1 ] = None
 let%test "three item list" = last_two [ 1; 2; 3 ] = Some (2, 3)
