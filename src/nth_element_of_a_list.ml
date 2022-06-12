eac19e51fe1c1514cfb41ff676053b43a8e86287
Josh Niec <ironick09@gmail.com>
2022-06-12 13:29:54 -0400

    problem 3 solution & test

--- src/nth_element_of_a_list.ml
+++ src/nth_element_of_a_list.ml
@@ -7,6 +7,13 @@ let rec elem (l : 'a list) (n : int) : 'a =
       | [] -> failwith "Index out of bounds"
       | _ :: rest -> elem rest (n - 1))
 
+(* correct solution *)
+let rec at n = function
+  | [] -> None
+  | h :: t -> if n = 0 then Some h else at (n - 1) t
+
 (* How to test for failure? *)
 (* let%test "empty list" = elem [] 2 = Failure "Index out of bounds" *)
 let%test "valid list & idx" = elem [ 6; 5; 4; 3; 2; 1; 0 ] 2 = 4
+let%test "correct: valid list & idx" = at 2 [ 6; 5; 4; 3; 2; 1; 0 ] = Some 4
+let%test "nth" = List.nth [ 6; 5; 4; 3; 2; 1; 0 ] 2 = 4
